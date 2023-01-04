5f47fe3f2169b30932416deb6e7b24f166db8e11
@irf23 <me@irfan.io>
2023-01-04 01:09:26 -0800

    - change order of plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,31 +21,6 @@ module.exports = {
         },
       },
     },
-    {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/static/img`,
-        name: "uploads",
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: "pages",
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images",
-      },
-    },
-    netlifyCmsPaths,
-    `gatsby-plugin-image`,
-    "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-transformer-remark",
@@ -74,8 +49,33 @@ module.exports = {
             },
           },
         ],
+      }
+    },
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/img`,
+        name: "images",
+      },
+    },
+    netlifyCmsPaths,
+    `gatsby-plugin-image`,
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
